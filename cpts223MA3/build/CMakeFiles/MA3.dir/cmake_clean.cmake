adaddb08b8a05a92dff2145c158307f60da7b2d6
ixlosh <146394480+ixlosh@users.noreply.github.com>
2024-10-27 20:29:13 +0000

    final commit on MA3

---
+++ cpts223MA3/build/CMakeFiles/MA3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MA3.dir/main.cpp.o"
+  "MA3"
+  "MA3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MA3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cpts223MA3/main.cpp
+++ cpts223MA3/main.cpp
@@ -2,72 +2,149 @@
 #include "generateUsers.hpp"
 using namespace std;
 
-
 map<string, User> buildMapByUserName(vector<User> users) {
     map<string, User> usersMap;
-    
-    cout << "TODO" << endl;
+
+    User vectorUser;
+
+    while (!users.empty()) {
+        vectorUser=users.back();
+        usersMap.insert(pair<string, User>(vectorUser.userName,vectorUser));
+        users.pop_back();
+    }
 
     return usersMap;
 }
-
 
 map<string, User> buildMapByEmail(vector<User> users) {
     map<string, User> usersMap;
     
-    cout << "TODO" << endl;
+    User vectorUser;
+
+    while (!users.empty()) {
+        vectorUser=users.back();
+        usersMap.insert(pair<string, User>(vectorUser.email,vectorUser));
+        users.pop_back();
+    }
 
     return usersMap;
 }
 
-
 void printMap(map<string, User> aMap) {
-    cout << "TODO" << endl;
+    int i=0;
+    for (auto pair : aMap) {
+        cout<<"#"<<i<<". Key: "<<pair.first<<", Value: ";
+        printAUser(pair.second);
+        i++;
+    }
 }
-
 
 bool isMapSorted(map<string, User> aMap) {
+    bool condition=false;
     if ( aMap.empty() )
-        return true;
-    
-    cout << "TODO" << endl;
-
-    return false;
+        condition = true;
+    else {
+        condition=true;
+        string lastAccessedKey;
+        for (map<string,User>::iterator it=aMap.begin();it!=aMap.end();it++) {
+            if (it==aMap.begin()) { // for the first element, no check is necessary
+                lastAccessedKey=it->first;
+            }
+            else {
+                if(lastAccessedKey>it->first) condition = false;
+                lastAccessedKey=it->first;
+            }
+        }
+    }
+    return condition;
 }
-
 
 bool testSearchByKey(map<string, User> aMap, string keyToSearch) {
-    cout << "TODO" << endl;
-    return false;
+    bool condition = false;
+    for (auto pair : aMap) {
+        if (pair.first==keyToSearch) condition = true;
+    }
+    return condition;
 }
-
 
 bool testDeleteByKey(map<string, User> aMap, string keyToDelete) {
-    cout << "TODO" << endl;
-    return false;
+    bool condition=false;
+    if (testSearchByKey(aMap,keyToDelete)) {
+        aMap.erase(keyToDelete);
+        if (!testSearchByKey(aMap,keyToDelete)) condition = true; // search again to confirm the deletion
+    }
+    return condition;
 }
-
 
 void printActiveUsers(map<string, User> aMap) {
     int activeThreshold = 800;
-    cout << "TODO" << endl;
+    int i=0;
+    for (auto pair : aMap) {
+        if (pair.second.numPosts>activeThreshold) {
+            cout<<"#"<<i<<". Key: "<<pair.first<<endl;
+        }
+        i++;
+    }
 }
 
+void printMostPopularCategory(map<string, User> aMap) {
+    int popularityCount[7] = {0};
+    // by index number
+    // 0: Technology
+    // 1: Sports
+    // 2: Music
+    // 3: Food
+    // 4: Travel
+    // 5: Education
+    // 6: Other
+    for (auto pair : aMap) {
+        if (pair.second.mostViewedCategory=="Technology") popularityCount[0]++;
+        else if (pair.second.mostViewedCategory=="Sports") popularityCount[1]++;
+        else if (pair.second.mostViewedCategory=="Music") popularityCount[2]++;
+        else if (pair.second.mostViewedCategory=="Food") popularityCount[3]++;
+        else if (pair.second.mostViewedCategory=="Travel") popularityCount[4]++;
+        else if (pair.second.mostViewedCategory=="Education") popularityCount[5]++;
+        else popularityCount[6]++;
+    }
 
-void printMostPopularCategory(map<string, User> aMap) {    
-    cout << "TODO" << endl;
+    int maxPopularity=-1;
+    for (int i=0;i<7;i++) {
+        if (popularityCount[i]>maxPopularity) maxPopularity=popularityCount[i];
+    }
+    
+    cout<<"Most Popular Categories with "<<maxPopularity<<" users->";
+
+    for (int m=0;m<7;m++) {
+        if (popularityCount[m]==maxPopularity) {
+            if (m==0) cout<<" Technology";
+            else if (m==1) cout<<" Sports";
+            else if (m==2) cout<<" Music";
+            else if (m==3) cout<<" Food";
+            else if (m==4) cout<<" Travel";
+            else if (m==5) cout<<" Education";
+            else if (m==6) cout<<" Other";
+        }
+    }
+
+    cout<<endl;
 }
-
 
 int main()
 {
     int numUsers = 10;
-    vector<User> users = generateUsers(numUsers);
+    
+    const string inFile = "../existingData.csv";
+    vector<User> users = readCSV(inFile);
+    int numInFile = (int)users.size();
 
+    int numToGenerate = numUsers-numInFile;
+    vector<User> randomUsers= generateUsers(numToGenerate);
+
+    users.insert(users.end(),randomUsers.begin(),randomUsers.end()); // combining to vectors to get the ultimate list
 
     cout << "Build map with username as key" << endl;
     map<string, User> mapByUserName = buildMapByUserName(users);
-    if ( mapByUserName.size() == numUsers )
+    if ( (int)mapByUserName.size() == numUsers )
         cout << "  Built successfully." << endl << endl;
     else
         cout << "  Built unsuccessfully." << endl << endl;
@@ -115,7 +192,7 @@ int main()
 
     cout << "Build map with username as key" << endl;
     map<string, User> mapByEmail = buildMapByEmail(users);
-    if ( mapByEmail.size() == numUsers )
+    if ( (int)mapByEmail.size() == numUsers )
         cout << "  Built successfully." << endl << endl;
     else
         cout << "  Built unsuccessfully." << endl << endl;
