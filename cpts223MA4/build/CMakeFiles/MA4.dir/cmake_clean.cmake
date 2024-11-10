12a223ed35b39130bab809f67f16241acf2e29f0
ixlosh <146394480+ixlosh@users.noreply.github.com>
2024-11-10 19:05:13 +0000

    first creation

---
+++ cpts223MA4/build/CMakeFiles/MA4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MA4.dir/main.cpp.o"
+  "MA4"
+  "MA4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MA4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
